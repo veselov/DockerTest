d1cc7f2280a7d37d88685dd85c68648dd808608d
Pawel Veselov <pawel.veselov@gmail.com>
2021-08-04 01:35:09 +0200

    Test case formed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,28 +2,39 @@ package org.example;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.BuildImageResultCallback;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.ExecCreateCmdResponse;
 import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.BuildResponseItem;
 import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.PullResponseItem;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.jaxrs.JerseyDockerHttpClient;
+import com.github.dockerjava.netty.NettyDockerCmdExecFactory;
 import com.github.dockerjava.okhttp.OkDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
+import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 public class App {
 
     private static DockerClient makeClient() {
         DockerClientConfig config = makeConfig();
-        DockerHttpClient dockerHttp = new OkDockerHttpClient.Builder().dockerHost(config.getDockerHost()).sslConfig(config.getSSLConfig()).build();
-        return DockerClientBuilder.getInstance(config).withDockerHttpClient(dockerHttp).build();
+        DockerHttpClient dockerHttp = new JerseyDockerHttpClient.Builder().dockerHost(config.getDockerHost()).sslConfig(config.getSSLConfig()).build();
+        return DockerClientBuilder.getInstance(config).withDockerHttpClient(dockerHttp).withDockerCmdExecFactory(new NettyDockerCmdExecFactory()).build();
     }
 
     private static DockerClientConfig makeConfig() {
@@ -39,9 +50,35 @@ public class App {
             System.out.println("Pulling image...");
             docker.pullImageCmd("alpine:latest").exec(new PullReport()).awaitCompletion();
 
+            File tempDir = File.createTempFile("javadocker", "dockerfile");
+            Files.delete(tempDir.toPath());
+            Files.createDirectories(tempDir.toPath());
+
+            File temp = File.createTempFile("javadocker", "dockerfile", tempDir);
+
+            try (FileWriter fw = new FileWriter(temp)) {
+                fw.write("FROM alpine:latest\n");
+                fw.write("RUN apk add strace\n");
+            }
+
+            System.out.println("Building image...");
+
+            BuildImageResultCallback bcr =
+                    docker.buildImageCmd()
+                            .withDockerfile(temp.getAbsoluteFile())
+                            .withBaseDirectory(tempDir)
+                            .exec(new BuildImageResultCallback() {
+                                @Override
+                                public void onNext(BuildResponseItem item) {
+                                    super.onNext(item);
+                                }
+                            });
+
+            String imageId = bcr.awaitImageId();
+
             System.out.println("Creating container...");
             CreateContainerResponse container = docker
-                    .createContainerCmd("alpine:latest")
+                    .createContainerCmd(imageId)
                     .withCmd("/bin/sh", "-c", "while true; do sleep 100; done")
                     .exec();
 
@@ -59,18 +96,44 @@ public class App {
             System.out.println("Executing echo...");
             ExecCreateCmdResponse execCmd = docker
                     .execCreateCmd(containerId)
-                    .withCmd("cat")
+                    // .withCmd("/bin/sh", "-c", "while true; do read -r line || break; sleep 1; echo $line; done")
+                    // .withCmd("cat")
+                    .withCmd("/usr/bin/strace", "-o", "/tmp/1.trs", "wc")
                     .withAttachStdin(true)
                     .withAttachStdout(true)
                     .withAttachStdin(true)
                     .exec();
 
+            // !!!!! Use inFile to see the problem, or new MyStream()
+            // with which everything is OK.
+            InputStream inFile = new GzipCompressorInputStream(Import.class.getResourceAsStream("/wtf.txt.gz"));
+            // InputStream inFile = new MyStream();
+
             AnyResultCallBack<Frame> cb = docker
                     .execStartCmd(execCmd.getId())
-                    .withStdIn(new MyStream())
+                    // .withStdIn(new MyStream())
+                    .withStdIn(inFile)
                     .exec(makeLogResult());
 
             cb.awaitCompletion();
+
+            System.out.println("Exec completed");
+
+            try (InputStream is = docker.copyArchiveFromContainerCmd(containerId, "/tmp/1.trs").exec();
+                 FileOutputStream fos = new FileOutputStream("1.trs.tar")) {
+
+                // $TODO: untar the file
+
+                byte [] data = new byte[16384];
+                while (true) {
+                    int nr = is.read(data);
+                    if (nr < 0) { break; }
+                    fos.write(data, 0, nr);
+                }
+
+            }
+
+            System.out.println("Copied?");
 
         } catch (Exception e) {
 
@@ -93,7 +156,7 @@ public class App {
 
     static class MyStream extends InputStream {
 
-        int i = 10;
+        int i = 1000000;
         byte [] data = null;
         int ptr;
 
@@ -109,7 +172,10 @@ public class App {
         public int read(byte[] b, int off, int len) throws IOException {
 
             if (data == null || ptr == data.length) {
-                if (i == 0) { return -1; }
+                if (i == 0) {
+                    System.out.println("<< STREAM EXHAUSTED");
+                    return -1;
+                }
                 data = (i + "\n").getBytes(StandardCharsets.UTF_8);
                 i--;
                 ptr = 0;
@@ -124,7 +190,7 @@ public class App {
 
         @Override
         public void close() {
-
+            System.out.println("<< STREAM CLOSED");
         }
     }
 
